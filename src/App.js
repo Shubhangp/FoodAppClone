3a40c52ad8f1c7537e5a7aa69529e0f0b2601bf7
Shubhangp <shubhanggpandey@gmail.com>
2023-08-02 16:01:11 +0530

    95% completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState, useContext } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Home";
